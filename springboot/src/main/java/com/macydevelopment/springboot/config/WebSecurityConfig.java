db8fa14ca78905a4bc3627a29e5603b4df696665
Macy Chan <macychan@student.ubc.ca>
2021-10-12 02:17:02 -0700

    Force using HTTPS

--- springboot/src/main/java/com/macydevelopment/springboot/config/WebSecurityConfig.java
+++ springboot/src/main/java/com/macydevelopment/springboot/config/WebSecurityConfig.java
@@ -7,51 +7,42 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 //import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
-
-
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+                http// .antMatcher("/**")
+                                .requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+                                .requiresSecure().and()
+                                .authorizeRequests(a -> a
+                                                .antMatchers("/", "/index**", "/Music**", "/login**", "/get**",
+                                                                "/callback/", "/webjars/**", "/error**",
+                                                                "/oauth2/authorization/**", "/db/**", "/test**")
+                                                .permitAll()
 
+                                                // .antMatchers("/MusicAndMood**")
+                                                // .permitAll()
+                                                .anyRequest().authenticated())
+                                .oauth2Login()
+                                /*
+                                 * oauth2Login -> oauth2Login.userInfoEndpoint( userInfoEndpointConfig ->
+                                 * userInfoEndpointConfig.oidcUserService( this.oidcUserService() ) ) )
+                                 */
+                                .and()
+                                // .csrf(c -> c
+                                // .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                                .csrf().disable()
+                                // )
+                                .formLogin().disable().logout().logoutSuccessUrl("/index.html").permitAll();
+        }
 
-        http//.antMatcher("/**")
-                .authorizeRequests( a ->
-                                    a.antMatchers("/", "/index**", "/Music**" ,"/login**", "/get**", "/callback/", "/webjars/**", "/error**", "/oauth2/authorization/**", "/db/**", "/test**")
-                                    .permitAll()
-
-
-                //.antMatchers("/MusicAndMood**")
-                //.permitAll()
-                                    .anyRequest()
-                                    .authenticated()
-                )
-                .oauth2Login()
-                       /* oauth2Login -> oauth2Login.userInfoEndpoint(
-                                userInfoEndpointConfig -> userInfoEndpointConfig.oidcUserService(
-                                        this.oidcUserService()
-                                )
-                        )
-                )*/
-                .and()
-                //.csrf(c -> c
-                //        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .csrf().disable()
-                //)
-                .formLogin().disable()
-                .logout().logoutSuccessUrl("/index.html").permitAll();
-    }
-
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web
-                .ignoring()
-                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**", "/fonts/**", "/scss/**","/vendor/**");
-    }
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+                web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**",
+                                "/fonts/**", "/scss/**", "/vendor/**");
+        }
 
 }
-
